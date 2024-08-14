53c26611d387df62bd8d830ed270a1064044147e
AriaRender <aria2crender@outlook.com>
2024-08-14 22:46:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,8 +35,8 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://aria2crender:Mgagzl38Tt582Qiq@cluster0.gzeih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-DB_NAME = environ.get("DB_NAME", "Hardik Parmar")
+DB_URI = environ.get("DB_URI", "mongodb+srv://hardik:hardik@7984087925@cluster0.p7x7d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "Cluster0")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', False)) # Set True or False
